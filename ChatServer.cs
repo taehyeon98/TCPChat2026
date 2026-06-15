3abbc2a9c625bbf4023289bc584251908d81656d
hyeontae12 <thkim980416@gmail.com>
2026-06-15 10:59:47 +0900

    feat:TCP기본세팅

---
+++ ChatServer.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TCPChatServer;
+
+public class ChatServer
+{
+    //TCP프로토콜을 사용해서 클라이언트가 접속하기를 대기하는 클래스 : Listener Server
+    
+    //소켓(Socket)
+    private TcpListener? _listener;
+    //서버 실행 여부
+    private bool _isRunning;
+    //서버 포트
+    private int _port;
+    
+    //생성자(Constructor)
+    public ChatServer(int port)
+    {
+        _port = port;
+        _isRunning = false;
+    }
+    
+    //서버 시작
+    public void StartServer()
+    {
+        if (_isRunning)
+        {
+            Console.WriteLine("서버 이미 실행중.");
+            return;
+        }
+        
+        //TCP 소켓 Open작업 = TCP 리스너 초기화
+        _listener = new TcpListener(IPAddress.Any,  _port);
+        _listener.Start();//소켓 메서드
+        _isRunning = true;
+        Console.WriteLine($"서버 정상 실행. 포트번호 : {_port} ");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿namespace ChatServer;
+﻿using System.Formats.Tar;
+
+namespace TCPChatServer;
+
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Windows 콘솔에서 UTF-8 인코딩 설정
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        
+        //기본 포트 번호 설정 0~65536
+        //1000번 이하 포트는 미리 예약되어 있다.
+        const int port = 7777;
+        
+        //채팅 서버 인스턴스 생성 및 실행.
+        var server = new ChatServer(port);
+        //서버 시작처리
+        server.StartServer();
+        
+        Console.WriteLine("서버 종료시 아무키 입력");
+        Console.ReadKey(true);//입력한 키 값 Hook, 화면에 출력되지 않게 하는 옵션
+        
+        //TODO:서버 정지 로직구현
+        Console.WriteLine("서버 종료.");
     }
 }
